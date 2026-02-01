4b6bcb28f691475bac49b83743c9bdba27348dd8
JuliaPalna <palna.julia@gmail.com>
2026-02-01 20:48:26 +0300

    add react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { Game } from './components/Game';
+import { store } from './store';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Game />
+        <Provider store={store}>
+            <Game />
+        </Provider>
     </StrictMode>,
 );
